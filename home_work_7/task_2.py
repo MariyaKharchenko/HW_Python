deaf5358c1c5c7ec99892dc9cc630fc1d61519e3
Mariya_Kharchenko <mariyaanatolevna86@yandex.ru>
2023-09-12 01:14:34 +0300

    changed task 2

--- home_work_7/task_2.py
+++ home_work_7/task_2.py
@@ -22,7 +22,7 @@
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for row in range(1, num_rows+1):
-        for column in range(1, num_columns):
+        for column in range(1, num_columns+1):
             res = operation(row, column)
             print(res, end=' ')   
         print('')
